79f54f133db492d3bc8f62855f677a8114c63a26
Marco Aguilera <marcoaguilera@Marcos-Mac.local>
2020-01-29 17:31:15 -0800

    First commit

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Marco Aguilera on 1/29/20.
+//  Copyright © 2020 Marco Aguilera. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    @IBOutlet weak var myImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tumblr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tumblr
+//
+//  Created by Marco Aguilera on 1/29/20.
+//  Copyright © 2020 Marco Aguilera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
